d515c3d573047cd6b286925ce932ae57d30491eb
adeepakumar <allam.deepak.kumar@takeda.com>
2026-07-21 15:04:09 +0000

    added ansible playbook

---
+++ learn-shell/comments.sh
@@ -0,0 +1,38 @@
+echo -e \\e[30mhello world\\e[0m
+echo -e \\e[31mhello world\\e[0m
+echo -e \\e[32mhello world\\e[0m
+echo -e \\e[33mhello world\\e[0m
+echo -e \\e[34mhello world\\e[0m
+echo -e \\e[35mhello world\\e[0m
+echo -e \\e[36mhello world\\e[0m
+echo -e \\e[37mhello world\\e[0m
+echo 
+echo -e \\e[40mhello world\\e[0m
+echo -e \\e[41mhello world\\e[0m
+echo -e \\e[42mhello world\\e[0m
+echo -e \\e[43mhello world\\e[0m
+echo -e \\e[44mhello world\\e[0m
+echo -e \\e[45mhello world\\e[0m
+echo -e \\e[46mhello world\\e[0m
+echo -e \\e[47mhello world\\e[0m
+echo 
+echo -e \\e[90mhello world\\e[0m
+echo -e \\e[91mhello world\\e[0m
+echo -e \\e[92mhello world\\e[0m
+echo -e \\e[93mhello world\\e[0m
+echo -e \\e[94mhello world\\e[0m
+echo -e \\e[95mhello world\\e[0m
+echo -e \\e[96mhello world\\e[0m
+echo -e \\e[97mhello world\\e[0m
+echo
+echo -e \\e[100mhello world\\e[0m
+echo -e \\e[101mhello world\\e[0m
+echo -e \\e[102mhello world\\e[0m
+echo -e \\e[103mhello world\\e[0m
+echo -e \\e[104mhello world\\e[0m
+echo -e \\e[105mhello world\\e[0m
+echo -e \\e[106mhello world\\e[0m
+echo -e \\e[107mhello world\\e[0m
+
+##########################################################
+
