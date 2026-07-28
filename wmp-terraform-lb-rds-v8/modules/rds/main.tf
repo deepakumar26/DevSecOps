9e4137eebab7485621e60d5eff8d61bf25e06048
adeepakumar <allam.deepak.kumar@takeda.com>
2026-07-28 17:01:00 +0000

    added

--- wmp-terraform-lb-rds-v8/modules/rds/main.tf
+++ wmp-terraform-lb-rds-v8/modules/rds/main.tf
@@ -80,7 +80,7 @@ resource "null_resource" "schema_load" {
 
       curl -fsSL \
         -o "${path.module}/global-bundle.pem" \
-        "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem"
+        "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" \
 
       PGPASSWORD='WmpUser#1234' /usr/pgsql-16/bin/psql \
         "host=${aws_db_instance.main.address} port=5432 dbname=default_dummy user=wmpuser sslmode=verify-full sslrootcert=${path.module}/global-bundle.pem" \
